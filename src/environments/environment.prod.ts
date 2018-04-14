76af29a7d03a24b1fbee4730441cd4ff3b9ecaf6
ShadowTapia <chelo.tapia@gmail.com>
2018-04-13 21:57:13 -0300

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCMpGnujbsiBb_9pbOUkjSdQXQppfraZCg",
+    authDomain: "jpsap-af33c.firebaseapp.com",
+    databaseURL: "https://jpsap-af33c.firebaseio.com",
+    projectId: "jpsap-af33c",
+    storageBucket: "jpsap-af33c.appspot.com",
+    messagingSenderId: "320089990319"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseConfig: {
+    apiKey: "AIzaSyCMpGnujbsiBb_9pbOUkjSdQXQppfraZCg",
+    authDomain: "jpsap-af33c.firebaseapp.com",
+    databaseURL: "https://jpsap-af33c.firebaseio.com",
+    projectId: "jpsap-af33c",
+    storageBucket: "jpsap-af33c.appspot.com",
+    messagingSenderId: "320089990319"
+  }
 };
