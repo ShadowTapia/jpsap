aa2ea09449a69187288d70a400838c7218cd1262
ShadowTapia <chelo.tapia@gmail.com>
2018-04-15 18:08:38 -0300

    Agregamos Guard rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomepageComponent} from './componentes/homepage/homepage.component';
-import {LoginPageComponent} from './componentes/login-page/login-page.component';
-import {RegisterPageComponent} from './componentes/register-page/register-page.component';
-import {MainPageComponent} from './componentes/main-page/main-page.component';
-import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { MainPageComponent } from './componentes/main-page/main-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent},
-  {path: 'login', component: LoginPageComponent},
-  {path: 'register', component: RegisterPageComponent},
-  {path: 'main', component: MainPageComponent},
-  {path: '**', component: NotFoundPageComponent}
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundPageComponent }
 ];
 
 @NgModule({
